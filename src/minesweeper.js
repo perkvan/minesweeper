cf5c3157b4ca01300c3124ced301c09fc7785add
Per Kvanbeck <per.kvanbeck@totalexpertinc.com>
2018-04-19 18:02:09 -0500

    Added space to Current Board title

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,5 @@
 const printBoard = board => {
-  console.log('Current Board:');
+  console.log('Current Board: ');
   console.log(board[0].join(' | '));
   console.log(board[1].join(' | '));
   console.log(board[2].join(' | '));
