398c10723c2d1271aef0478d05fa4b6dfe37a1dd
Per Kvanbeck <per.kvanbeck@totalexpertinc.com>
2018-05-06 12:55:58 -0500

    Changed comment style to be multiple line.

--- src/game.js
+++ src/game.js
@@ -1,14 +1,14 @@
 import { Board } from './board';
 
-// To play Minesweeper, we will create instances of MineSweeperGame in command line.
-// For example:
-// In the command line, navigate to the lib directory and run `node`
-// Run `.load game.js` to load the contents of this file.
-// Then create a Game instance and run commands like so:
-// let game = new Game(3, 3, 3);
-// game.playMove(0, 1);
-// game.playMove(1, 2);
-// When done run `.exit`
+/*To play Minesweeper, we will create instances of MineSweeperGame in command line.
+For example:
+In the command line, navigate to the lib directory and run `node`
+Run `.load game.js` to load the contents of this file.
+Then create a Game instance and run commands like so:
+let game = new Game(3, 3, 3);
+game.playMove(0, 1);
+game.playMove(1, 2);
+When done run `.exit`*/
 
 class Game {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
